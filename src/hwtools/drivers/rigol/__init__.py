67dceae93d80d76d697cb73d8ad0292f731af22d
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2026-07-31 16:03:12 +0100

    Add the Rigol DG1062Z signal-generator driver

--- src/hwtools/drivers/rigol/__init__.py
+++ src/hwtools/drivers/rigol/__init__.py
@@ -1,5 +1,6 @@
 """Rigol instrument drivers."""
 
+from hwtools.drivers.rigol.dg1062 import DG1062
 from hwtools.drivers.rigol.ds1054z import DS1054Z
 
-__all__ = ["DS1054Z"]
+__all__ = ["DG1062", "DS1054Z"]
